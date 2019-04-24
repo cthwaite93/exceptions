2700331a8a1a792c574de01309967b65a39d01b5
cthwaite <crivasi@xtec.cat>
2019-04-24 16:36:06 +0200

    Added example that propagates exception till catched

--- Examples.java
+++ Examples.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class Examples {
 	private static void printBuffer(BufferedReader bf) throws IOException {
 		String n = bf.readLine(); //can generate an exception
+		System.out.println(n);
 	}
 	
 	private static void writeRead() {
@@ -16,7 +17,35 @@ public class Examples {
 		}
 	}
 	
+    private static int divideByZero(int a, int b){  
+        int i = a/b;  
+          
+        return i; 
+    } 
+      
+    private static int computeDivision(int a, int b) {    
+        int res =0; 
+        try
+        { 
+          res = divideByZero(a,b); 
+        } catch(NumberFormatException ex) { 
+           System.out.println("NumberFormatException is occured");  
+        } 
+        return res; 
+    }
+    
+    private static void joyDivision() {
+    	int a = 1;
+    	int b = 0;
+    	try {
+    		System.out.println(computeDivision(a, b));
+    	} catch (ArithmeticException e) {
+    		System.out.println(e.getMessage());
+    	}
+    }
+	
 	public static void main(String[] args) {
 		writeRead();
+		joyDivision();
 	}
 }
