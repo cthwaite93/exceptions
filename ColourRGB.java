3c7c11650d1e2fa3ab2902ea62bd5d9659282f96
cthwaite <crivasi@xtec.cat>
2019-04-25 09:43:13 +0200

    Completed ColourRGB with methods that throw exception

--- ColourRGB.java
+++ ColourRGB.java
@@ -5,13 +5,40 @@ public class ColourRGB {
 	private int g;
 	private int b;
 	
-	public ColourRGB(int r, int g, int b) {
+	private String createExceptionMessage(int r, int g, int b) {
+		String s = "";
+		if (r < 0 || r > 255) {
+			s += "Value R = " + r + " out of bounds. ";
+		}
+		
+		if (g < 0 || g > 255) {
+			s += "Value G = " + g + " out of bounds. ";
+		}
+		
+		if (b < 0 || b > 255) {
+			s += "Value B = " + b + " out of bounds. ";
+		}
+		
+		return s;
+	}
+	
+	public ColourRGB(int r, int g, int b) throws ColourRGBException {
+		if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+			String s = createExceptionMessage(r, g, b);
+			throw new ColourRGBException(s);
+		}
+		
 		this.r = r;
 		this.g = g;
 		this.b = b;
 	}
 	
-	public void setColour(int r, int g, int b) {
+	public void setColour(int r, int g, int b) throws ColourRGBException {
+		if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+			String s = createExceptionMessage(r, g, b);
+			throw new ColourRGBException(s);
+		}
+		
 		this.r = r;
 		this.g = g;
 		this.b = b;
@@ -33,16 +60,25 @@ public class ColourRGB {
 		return r;
 	}
 	
-	public void setB(int b) {
-		this.b = b;
+	public void setR(int r) throws ColourRGBException {
+		if (r < 0 || r > 255) {
+			throw new ColourRGBException("Value R = " + r + " out of bounds");
+		}
+		this.r = r;
 	}
 	
-	public void setG(int g) {
+	public void setG(int g) throws ColourRGBException {
+		if (g < 0 || g > 255) {
+			throw new ColourRGBException("Value G = " + g + " out of bounds");
+		}
 		this.g = g;
 	}
 	
-	public void setR(int r) {
-		this.r = r;
+	public void setB(int b) throws ColourRGBException {
+		if (b < 0 || b > 255) {
+			throw new ColourRGBException("Value B = " + b + " out of bounds");
+		}
+		this.b = b;
 	}
 	
 }
